2676cdb3e5c4514b065287aed538e50e32d9633e
pra99424 <pradeep99424@gmail.com>
2023-02-17 17:58:40 +0530

    07-02-2023

--- HTML/JavaScript/Day_1/introduction.js
+++ HTML/JavaScript/Day_1/introduction.js
@@ -1,11 +1,12 @@
-const para = document.querySelector('p');
-para.addEventListener('click', updateName);
-function updateName() {
-    const name = prompt('Enter a new name');
-    para.textContent = `NAME: ${name}`;
-}
 
- alert("Welcome");
+// const para = document.querySelector('p');
+// para.addEventListener('click', updateName);
+// function updateName() {
+//     const name = prompt('Enter a new name');
+//     para.textContent = `NAME: ${name}`;
+// }
+
+// alert("Welcome");
 
 const name = 'Bingo';
 console.log ('name');
@@ -15,64 +16,73 @@ const greeting = 'name' + 'hello';
 console.log("greeting") ;
 
 console.log(2 + 2);
-// Expected output: 4
+
 
 console.log(2 + true);
-// Expected output: 3
+
 
 console.log('hello ' + 'everyone');
-// Expected output: "hello everyone"
+
 
 console.log(2001 + ': A Space Odyssey');
-// Expected output: "2001: A Space Odyssey"
+
 
 let a = 2;
 let b = 'hello';
+// Addition by anonymous '+' operator in prefix of equal to  
+console.log(a += 3);
 
-console.log(a += 3); // Addition
-// Expected output: 5
+// Concatenation also done the this format using string
+console.log(b += ' world'); 
 
-console.log(b += ' world'); // Concatenation
-// Expected output: "hello world"
 let x = 2;
 const y = 3;
 
 console.log(x);
-// Expected output: 2
+console.log(x = y + 1);
+//multiply * operator
+console.log(x = x * y); 
 
-console.log(x = y + 1); // 3 + 1
-// Expected output: 4
-
-console.log(x = x * y); // 4 * 3
-// Expected output: 12
 
 //DOM AbortController
 let controller;
 const url = "video.mp4";
 
-const downloadBtn = document.querySelector(".download");
-const abortBtn = document.querySelector(".abort");
+// const downloadBtn = document.querySelector(".download");
+// const abortBtn = document.querySelector(".abort");
 
-downloadBtn.addEventListener("click", fetchVideo);
+// downloadBtn.addEventListener("click", fetchVideo);
 
-abortBtn.addEventListener("click", () => {
-  if (controller) {
-    controller.abort();
-    console.log("Download aborted");
-  }
-});
+// abortBtn.addEventListener("click", () => {
+//   if (controller) {
+//     controller.abort();
+//     console.log("Download aborted");
+//   }
+// });
 
-function fetchVideo() {
-  controller = new AbortController();
-  const signal = controller.signal;
-  fetch(url, { signal })
-    .then((response) => {
-      console.log("Download complete", response);
-    })
-    .catch((err) => {
-      console.error(`Download error: ${err.message}`);
-    });
-}
+// function fetchVideo() {
+//   controller = new AbortController();
+//   const signal = controller.signal;
+//   fetch(url, { signal })
+//     .then((response) => {
+//       console.log("Download complete", response);
+//     })
+//     .catch((err) => {
+//       console.error(`Download error: ${err.message}`);
+//     });
+// }
 //using innerHTML and GetElementById we can see the text in webpage
-let text = "Pradeep T";  // String written inside quotes
-document.getElementById("texting in website").innerHTML = text;
+// let text = "Pradeep T";  // String written inside quotes
+// document.getElementById("texting in website").innerHTML = text;
+
+/* datatype */
+
+
+console.log(Number.isFinite(1 / 0));
+// Expected output: false
+
+console.log(Number.isFinite(10 / 5));
+// Expected output: true
+
+console.log(Number.isFinite(0 / 0));
+// Expected output: false
