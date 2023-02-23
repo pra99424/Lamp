53e95f71c3f56c6dcfbadcc9f1f4e206fa4c2fe9
Pradeep Thirumoorthy <pradeep99424@gmail.com>
2023-02-23 21:04:36 +0530

    23.02.2023

--- JavaScript/javascript.js
+++ JavaScript/javascript.js
@@ -698,3 +698,77 @@ function myFunc() {
   para.textContent = 'You clicked ';
   document.body.appendChild(para);
 }
+//OOPS
+walsh = new Professor("Walsh", "Psychology");
+lillian = new Professor("Lillian", "Poetry");
+
+walsh.teaches; 
+walsh.introduceSelf(); 
+
+lillian.teaches; 
+lillian.introduceSelf(); 
+
+//Inhertince
+class Professor extends Person {
+
+  teaches;
+
+  constructor(name, teaches) {
+    super(name);
+    this.teaches = teaches;
+  }
+
+  introduceSelf() {
+    console.log(`My name is ${this.name}, and I will be your ${this.teaches} professor.`);
+  }
+
+  grade(paper) {
+    const grade = Math.floor(Math.random() * (5 - 1) + 1);
+    console.log(grade);
+  }
+
+}
+
+walsh = new Professor("Walsh", "Psychology");
+walsh.introduceSelf(); 
+
+summers = new Student("Summers", 1);
+summers.introduceSelf(); 
+
+pratt = new Person("Pratt");
+pratt.introduceSelf(); 
+
+//Encapsulation
+function Student(name,marks)  
+{  
+  var s_name=name;  
+  var s_marks=marks;  
+  Object.defineProperty(this,"name",{  
+    get:function()  
+    {  
+      return s_name;  
+    },  
+  set:function(s_name)  
+  {  
+    this.s_name=s_name;  
+  }  
+    
+});  
+   
+    Object.defineProperty(this,"marks",{  
+    get:function()  
+    {  
+      return s_marks;  
+    },  
+  set:function(s_marks)  
+  {  
+    this.s_marks=s_marks;  
+  }  
+    
+});  
+    
+}  
+  var stud=new Student("John",80);  
+  document.writeln(stud.name+" "+stud.marks);  
+
+  //
--- README.md
+++
@@ -1,2 +0,0 @@
-# Lamp
-HTML,CSS,JAVA SCRIPT
